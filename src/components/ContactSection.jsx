76f37d79a860f15049c7eff00cdce5bed18c7760
E-commerce-React-app <09iqrasiddique.com>
2025-05-07 12:42:15 +0500

    some changes

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,7 +3,7 @@ import { CONTACT } from '../constants'
 import {motion} from "framer-motion"
 const ContactSection = () => {
   return (
-   <section className='container mx-auto py-16 ' id='content'>
+   <section className='container mx-auto py-16 ' id='contact'>
     <h2 className='mb-8 text-center text-3xl lg:text-4xl'>Contact Us</h2>
     <div className='text-neutral-400'>
         {CONTACT.map((detail) =>(
--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { AiFillStar } from "react-icons/ai"; 
 const DishCard = ({project}) => {
   return (
     <div>
@@ -10,8 +10,14 @@ const DishCard = ({project}) => {
         <h3 className='mb-2 text-2xl font-bold tracking-tighter'>
             {project.title}
             </h3>
-          <p className='text-sm'>{project.description}</p>
-    </div>
+          <p className='text-sm'>{project.description}</p> 
+              </div>
+              <div className='flex justify-between flex-wrap p-4'>  
+               
+                  <h3 className=' mb-4 text-2xl font-bold tracking-tighter text-green-600 '> $ {project.price}</h3>
+           <button className='bg-transparent w-full hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded  '>Add to cart</button>
+
+              </div> 
      </div>
   )
 }
